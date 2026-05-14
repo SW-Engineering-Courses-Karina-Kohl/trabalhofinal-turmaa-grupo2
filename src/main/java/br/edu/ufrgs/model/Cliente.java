b1f5ed95b50dae5038e479bf5f6c344c48f5d537
Marina <marinamoli@correo.ugr.es>
2026-05-14 15:47:13 -0300

    criaçao de clase calculadora

--- src/main/java/br/edu/ufrgs/model/Cliente.java
+++ src/main/java/br/edu/ufrgs/model/Cliente.java
@@ -1,5 +1,38 @@
 package br.edu.ufrgs.model;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class Cliente {
-    
+public class Cliente {
+    private int idCliente;
+    private String nome;
+    private List<Venda> vendas = new ArrayList<>();
+
+    public Cliente(int idCliente, String nome) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+    }
+
+    public void adicionarVenda(Venda v) {
+        vendas.add(v);
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Venda> getVendas() {
+        return vendas;
+    }
+
+    public double getValorTotalVendas() {
+        double total = 0;
+        for (Venda v : vendas) {
+            total += v.getValor();
+        }
+        return total;
+    }
 }
--- src/main/java/br/edu/ufrgs/model/Venda.java
+++ src/main/java/br/edu/ufrgs/model/Venda.java
@@ -1,5 +1,25 @@
 package br.edu.ufrgs.model;
 
 public class Venda {
-    
+    private String idVenda;
+    private double valor;
+    private String categoria;
+
+    public Venda(String idVenda, double valor, String categoria) {
+        this.idVenda = idVenda;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public String getIdVenda() {
+        return idVenda;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
 }
