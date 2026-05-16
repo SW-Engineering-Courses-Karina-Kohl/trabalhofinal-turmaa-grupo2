6017e32d633ff9893a8a102caa40dfac1b0a61b1
esthevann <esthevanferretti69@gmail.com>
2026-05-16 18:45:50 -0300

    estruturação classes filho de cliente, gerenciador de arquivos e CashbackService

---
+++ src/main/java/br/edu/ufrgs/model/ClienteNormal.java
@@ -0,0 +1,9 @@
+package br.edu.ufrgs.model;
+
+public class ClienteNormal extends Cliente {
+
+    public ClienteNormal(int idCliente, String nome) {
+        super(idCliente, nome);
+    }
+    
+}
---
+++ src/main/java/br/edu/ufrgs/persistence/GerenciadorDeArquivos.java
@@ -0,0 +1,24 @@
+package br.edu.ufrgs.persistence;
+
+import br.edu.ufrgs.model.Cliente;
+import br.edu.ufrgs.model.Venda;
+import java.util.List;
+
+public class GerenciadorDeArquivos {
+    
+    private String separador;
+
+
+    public GerenciadorDeArquivos(String separador) {
+        this.separador = separador;
+    }
+
+    public List<Venda> lerVendas(String caminhoArquivo) {
+        // Lógica para ler o CSV de entrada 
+        return null; 
+    }
+
+    public void exportarRelatorio(List<Cliente> clientes, String caminhoSaida) {
+        // Lógica para gravar o CSV de saída consolidado 
+    }
+}
